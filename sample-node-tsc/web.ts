51044eac4d714693b695d0b87b0195f7e44e8ac0
James Ward <james@jamesward.com>
2022-12-14 07:35:43 -0700

    Update sample-node-tsc/web.ts

--- sample-node-tsc/web.ts
+++ sample-node-tsc/web.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello, TS world');
+  res.send('hello, world');
 });
 
 app.listen(process.env.PORT || 8080);
