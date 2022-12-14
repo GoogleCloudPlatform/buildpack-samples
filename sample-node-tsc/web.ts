90393055607045b736af1b20c0a89ebfedbb2347
dysbulic <dys@dhappy.org>
2022-12-13 23:45:38 -0500

    setting an existing Node version & differentiating output

--- sample-node-tsc/web.ts
+++ sample-node-tsc/web.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello, world');
+  res.send('hello, TS world');
 });
 
 app.listen(process.env.PORT || 8080);
