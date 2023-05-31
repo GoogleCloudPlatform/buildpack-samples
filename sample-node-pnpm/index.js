16bd57060f9010e06296c8b14fcffbfbe39d1964
Matthew Robertson <mattrobertson@google.com>
2023-05-31 16:26:46 +0000

    add pnpm sample to README.md

--- sample-node-pnpm/index.js
+++ sample-node-pnpm/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('hello, world');
 });
 
-const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
